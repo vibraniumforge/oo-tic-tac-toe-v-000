e4c8b9a2b19898a9587fa10b0bc9f17e38f643c5
Greg Morgan <gregm9999@gmail.com>
2018-10-30 21:57:06 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -79,7 +79,7 @@ def current_player
   turn_count % 2 == 0 ? "X" : "O"
 end
 
-def won?(board)
+def won?
   WIN_COMBINATIONS.each do |win_combination|
     win_index_1=win_combination[0]
     win_index_2=win_combination[1]
