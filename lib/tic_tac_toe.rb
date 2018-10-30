ce33163e7d14ba38031d0d939809152fe003bbbe
Greg Morgan <gregm9999@gmail.com>
2018-10-30 21:11:47 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -85,9 +85,9 @@ def won?
     win_index_2=win_combination[1]
     win_index_3=win_combination[2]
 
-    position_1=board[win_index_1]
-    position_2=board[win_index_2]
-    position_3=board[win_index_3]
+    position_1=@board[win_index_1]
+    position_2=@board[win_index_2]
+    position_3=@board[win_index_3]
     puts "won? fires"
     if position_1==position_2 && position_2==position_3 && position_1 !=" "
       puts"won? true fires"
