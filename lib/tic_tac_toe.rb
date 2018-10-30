7920504c020a2fe7418084dfd1f3483d6fe20074
Greg Morgan <gregm9999@gmail.com>
2018-10-30 21:25:26 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -64,7 +64,7 @@ def turn
   input = gets.strip
   index=input_to_index(input)
   if valid_move?(@board, index)
-    move(@board, index, character)
+    move(@board, index, current_player(@board))
     display_board(@board)
   else
     turn(@board)
