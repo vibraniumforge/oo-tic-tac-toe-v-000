32cd602decbd5e0fac815c866914018ab09b3384
Greg Morgan <gregm9999@gmail.com>
2018-10-30 21:21:02 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -51,7 +51,7 @@ def position_taken?(index)
     end
 end
 
-def valid_move?( index)
+def valid_move?( board, index)
   if index.between?(0,8) && !position_taken?(@board, index)
     return true
   else
