b24d2333c9548d02b8c18eeba81702f070a76b65
Greg Morgan <gregm9999@gmail.com>
2018-10-30 21:53:01 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -89,7 +89,7 @@ def won?
     position_2=@board[win_index_2]
     position_3=@board[win_index_3]
     if position_1==position_2 && position_2==position_3 && position_1 !=" "
-      return win_combination[0]
+      return win_combination
     end
   end
   return false
@@ -131,9 +131,8 @@ end
 
 def winner
   if won?
-    # z=@board[won?[0]]
-    # return z
-    return won?
+    z=@board[won?[0]]
+    return z
   end
 end
 
